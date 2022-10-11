37e01644378fd051b682d8708319d9efda2927f8
Rev-Schnabel <Rev-Schnabel@users.noreply.github.com>
2022-10-11 22:43:18 +0900

    Add navigation and home.

---
+++ src/home/home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Navigation } from "../common/navigation";
+
+export const Home: React.FC = () => {
+  return (
+    <div>
+      <Navigation />
+      hello!
+    </div>
+  );
+};
