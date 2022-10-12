7b87f7ceeaa1b4142dec81244ba1fcd30eb35f07
Rev-Schnabel <Rev-Schnabel@users.noreply.github.com>
2022-10-12 20:41:34 +0900

    Add other links.

--- src/archive/archive.tsx
+++ src/archive/archive.tsx
@@ -51,7 +51,11 @@ export const Archive: React.FC = () => {
           <Divider variant="middle" sx={{ m: 1 }}/>
           <Typography variant="body1" gutterBottom>
             現在の神殿は以下に建てられている。<br /><br />
-            <a style={{ textDecoration: "none" }} href="https://jbbs.shitaraba.net/bbs/read.cgi/study/12015/1363951037/l50">（　＾ω＾）おにんにん教</a>
+            <a style={{ textDecoration: "none" }} href="https://jbbs.shitaraba.net/bbs/read.cgi/study/12015/1363951037/l50">（　＾ω＾）おにんにん教</a><br /><br />
+            もし興味があれば、正式な神殿以外の資料も確認すると良い。もっとも、魑魅魍魎の作成したものも多数あるが…。<br /><br />
+            <a style={{ textDecoration: "none" }} href="https://kakolog.jp/?q=%E3%81%8A%E3%81%AB%E3%82%93%E3%81%AB%E3%82%93%E6%95%99&d=2012">かころぐβ</a><br />
+            <a style={{ textDecoration: "none" }} href="https://jbbs.shitaraba.net/study/12015/">聖おにんにん教したらば神殿</a><br />
+            <a style={{ textDecoration: "none" }} href="https://jbbs.shitaraba.net/bbs/subject.cgi/study/12015/?q=%E3%81%8A%E3%81%AB%E3%82%93%E3%81%AB%E3%82%93%E6%95%99">したらば掲示板検索</a><br />
           </Typography>
         </Paper>
       </Box>
